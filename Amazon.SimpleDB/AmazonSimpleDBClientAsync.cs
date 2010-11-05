ce755c34cfbb8d435ec3eacb605987592b6ffec0
David Pio <david.pio@crownpeak.com>
2010-11-05 15:00:34 -0700

    Update license text to be more accurate

--- Amazon.SimpleDB/AmazonSimpleDBClientAsync.cs
+++ Amazon.SimpleDB/AmazonSimpleDBClientAsync.cs
@@ -1,17 +1,26 @@
 ﻿/*******************************************************************************
- * Copyright 2010-2010 CrownPeak Technology. or its affiliates. All Rights Reserved.
+ * Copyright 2008-2010 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
  * this file except in compliance with the License. A copy of the License is located at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http://aws.amazon.com/apache2.0
  *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  * *****************************************************************************
- * 
- *  Author: David Pio
+ *    __  _    _  ___
+ *   (  )( \/\/ )/ __)
+ *   /__\ \    / \__ \
+ *  (_)(_) \/\/  (___/
+ *
+ *  AWS SDK for .NET
+ *  API Version: 2009-04-15
+ */
+
+/*
+    Copied most of it out of AmazonSimpleDBClient and then made the async mods - David Pio 
  */
 
 using System;
